4fc13454caefc3c80c59ee1e896011eaee085229
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2004-02-24 08:41:57 +0000

    added storage.properties module for handling properties files...
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@49 54714841-351b-0410-a198-e36a94b762f5

---
+++ storage/properties.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# 
+# Copyright 2002, 2003 Zuza Software Foundation
+# 
+# This file is part of translate.
+#
+# translate is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# translate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with translate; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""classes that hold elements of .properties files (propelement) or entire files (propfile)
+these files are used in translating Mozilla and other software"""
+
+from translate.misc import quote
+
+# the rstripeols convert dos <-> unix nicely as well
+# output will be appropriate for the platform
+
+eol = "\n"
+
+class propelement:
+  """an element of a properties file i.e. a name and value, and any comments associated"""
+  def __init__(self):
+    """construct a blank propelement"""
+    self.name = ""
+    self.msgid = ""
+    self.comments = []
+
+  def tolines(self):
+    """convert the element back into formatted lines for a .properties file"""
+    return self.comments + ["%s=%s\n" % (self.name, self.msgid)]
+
+class propfile:
+  """this class represents a .properties file, made up of propelements"""
+  def __init__(self, inputfile=None):
+    """construct a pofile, optionally reading in from inputfile"""
+    self.propelements = []
+    self.filename = getattr(inputfile, 'name', '')
+    if inputfile is not None:
+      proplines = inputfile.readlines()
+      inputfile.close()
+      self.fromlines(proplines)
+
+  def fromlines(self, lines):
+    """read the lines of a po file in and include them as poelements"""
+    newpe = propelement()
+    inmultilinemsgid = 0
+    for line in lines:
+      finished = 0
+      # handle multiline msgid if we're in one
+      if inmultilinemsgid:
+        newpe.msgid += quote.rstripeol(line).lstrip()
+        # see if there's more
+        inmultilinemsgid = (newpe.msgid[-1:] == '\\')
+        # if we're still waiting for more...
+        if inmultilinemsgid:
+          # strip the backslash
+          newpe.msgid = newpe.msgid[:-1]
+        if not inmultilinemsgid:
+          # we're finished, add it to the list...
+          self.propelements.append(newpe)
+          newpe = propelement()
+      # otherwise, this could be a comment
+      elif line.strip()[:1] == '#':
+        # add a comment
+        newpe.comments.append(line)
+      else:
+        equalspos = line.find('=')
+        # if no equals, just ignore it
+        if equalspos == -1:
+          continue
+        # otherwise, this is a definition
+        else:
+          newpe.name = line[:equalspos].strip()
+          newpe.msgid = quote.rstripeol(line[equalspos+1:]).lstrip()
+          # backslash at end means carry string on to next line
+          if newpe.msgid[-1:] == '\\':
+            inmultilinemsgid = 1
+            newpe.msgid = newpe.msgid[:-1]
+          else:
+            self.propelements.append(newpe)
+            newpe = propelement()
+    # see if there is a leftover one...
+    if inmultilinemsgid or len(newpe.comments) > 0:
+      self.propelements.append(newpe)
+
+  def tolines(self):
+    """convert the propelements back to lines"""
+    lines = []
+    for pe in self.propelements:
+      pelines = pe.tolines()
+      lines.extend(pelines)
+    return lines
+
+if __name__ == '__main__':
+  import sys
+  pf = propfile(sys.stdin)
+  sys.stdout.writelines(pf.tolines())
+
