65a4e9a97f4f733cb5f47408af74ed8fb0e3e441
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2004-02-24 17:12:02 +0000

    made the csv setup script usable by other setup scripts
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@72 54714841-351b-0410-a198-e36a94b762f5

--- misc/setup.py
+++ misc/setup.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
+import os
 
-csvModule = Extension('_csv', ['_csv.c'])
+class csvExtension(Extension):
+  def __init__(self, source_dir):
+    Extension.__init__(self, '_csv',
+                    sources = [os.path.join(source_dir, '_csv.c')])
 
-setup(name="_csv",version="2.3b",description="csv module",py_modules=['csv'],ext_modules=[csvModule])
+csvModule = csvExtension('.')
+
+if __name__ == "__main__":
+  setup(name="_csv",version="2.3b",description="csv module",py_modules=['csv'],ext_modules=[csvModule])
 
