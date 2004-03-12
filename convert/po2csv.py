6a590edea14b70364e4b5cd77e7c55647840443c
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2004-03-12 13:15:02 +0000

    converted totally to convert.ConvertOptionParser system
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@114 54714841-351b-0410-a198-e36a94b762f5

--- convert/po2csv.py
+++ convert/po2csv.py
@@ -25,8 +25,6 @@ import os
 from translate.storage import po
 from translate.storage import csvl10n
 from translate import __version__
-# FIXME: convert to convert
-import optparse
 
 class po2csv:
   def convertstrings(self,thepo,thecsv):
@@ -61,80 +59,28 @@ class po2csv:
         thecsvfile.csvelements.append(thecsv)
     return thecsvfile
 
-def convertfile(inputfile, outputfile):
+def convertcsv(inputfile, outputfile, templatefile):
   """reads in inputfile using po, converts using po2csv, writes to outputfile"""
+  # note that templatefile is not used, but it is required by the converter...
   inputpo = po.pofile(inputfile)
+  if inputpo.isempty():
+    return 0
   convertor = po2csv()
   outputcsv = convertor.convertfile(inputpo)
   outputcsvlines = outputcsv.tolines()
   outputfile.writelines(outputcsvlines)
-
-def recurse(inputdir, outputdir, inputformat, outputformat):
-  """recurse through inputdir and convertfiles in inputformat to outputformat in outputdir"""
-  dirstack = ['']
-  while dirstack:
-    top = dirstack.pop(-1)
-    names = os.listdir(os.path.join(inputdir, top))
-    dirs = []
-    for name in names:
-      inputname = os.path.join(inputdir, top, name)
-      # handle directories...
-      if os.path.isdir(inputname):
-        dirs.append(os.path.join(top, name))
-        outputname = os.path.join(outputdir, top, name)
-        if not os.path.isdir(outputname):
-          os.mkdir(outputname)
-      elif os.path.isfile(inputname):
-        # only handle names that match the correct extension...
-        base, inputext = os.path.splitext(name)
-        if inputext != os.extsep + inputformat:
-          print >>sys.stderr, "not processing %s: wrong extension (%r != %r)" % (name, inputext, inputformat)
-          continue
-        outputname = os.path.join(outputdir, top, base) + os.extsep + outputformat
-        inputfile = open(inputname, 'r')
-        outputfile = open(outputname, 'w')
-        convertfile(inputfile, outputfile)
-    # make sure the directories are processed next time round...
-    dirs.reverse()
-    dirstack.extend(dirs)
-
-def handleoptions(options, inputformat, outputformat):
-  """handles the options, allocates files, and runs the neccessary functions..."""
-  if options.recursive:
-    if options.pot:
-      inputformat = "pot"
-    if options.input is None:
-      raise optparse.OptionValueError("cannot use stdin for recursive run. please specify inputfile")
-    if not os.path.isdir(options.input):
-      raise optparse.OptionValueError("inputfile must be directory for recursive run.")
-    if options.output is None:
-      raise optparse.OptionValueError("must specify output directory for recursive run.")
-    if not os.path.isdir(options.output):
-      raise optparse.OptionValueError("output must be existing directory for recursive run.")
-    recurse(options.input, options.output, inputformat, outputformat)
-  else:
-    if options.input is None:
-      inputfile = sys.stdin
-    else:
-      inputfile = open(options.input, 'r')
-    if options.output is None:
-      outputfile = sys.stdout
-    else:
-      outputfile = open(options.output, 'w')
-    convertfile(inputfile, outputfile)
+  return 1
 
 if __name__ == '__main__':
   # handle command line options
   from translate.convert import convert
-  inputformat = "po"
+  inputformat = {"po":convertcsv}
   outputformat = "csv"
-  parser = convert.ConvertOptionParser(convert.optionalrecursion, inputformat, outputformat)
-  parser.add_option("-P", "--pot", action="store_true", dest="pot", default=False, \
-                    help="convert PO template (.pot) with blank msgstrs")
+  parser = convert.ConvertOptionParser(convert.optionalrecursion, inputformat, outputformat, usepots=True)
   (options, args) = parser.parse_args()
   # open the appropriate files
   try:
-    handleoptions(options, inputformat, outputformat)
-  except optparse.OptParseError, message:
+    parser.runconversion(options, None)
+  except convert.optparse.OptParseError, message:
     parser.error(message)
 
