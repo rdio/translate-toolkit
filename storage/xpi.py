f44e43e979dd6f85a71cbc48b9868517933d1d9a
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2004-04-08 09:04:15 +0000

    a lot of refactoring extracted _commonprefix method added a XpiFile constructor that reads out the important information cache jarfiles in self.jarfiles so they aren't extracted repeatedly added stripcommonprefix, findjarprefixes methods added ziptoospath, ostozippath methods this all simplifies iterextractnames a lot
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@195 54714841-351b-0410-a198-e36a94b762f5

--- storage/xpi.py
+++ storage/xpi.py
@@ -28,68 +28,98 @@ try:
 except:
   from StringIO import StringIO
 
+def _commonprefix(filenamelist):
+  filelist = []
+  for filename in filenamelist:
+    filelist.append(filename.split('/'))
+  def cp(a, b):
+    l = min(len(a), len(b))
+    for n in range(l):
+      if a[n] != b[n]: return a[:n]
+    return a[:l]
+  return reduce(cp, filelist)
+
 class XpiFile(zipfile.ZipFile):
+  def __init__(self, *args, **kwargs):
+    """sets up the xpi file"""
+    zipfile.ZipFile.__init__(self, *args, **kwargs)
+    self.jarfiles = {}
+    self.commonprefix = self.findcommonprefix()
+    self.jarprefixes = self.findjarprefixes()
+
   def iterjars(self):
     """iterate through the jar files in the xpi as ZipFile objects"""
     for filename in self.namelist():
       if filename.lower().endswith('.jar'):
-        jarcontents = self.read(filename)
-        jarstream = StringIO(jarcontents)
-        jarfile = zipfile.ZipFile(jarstream)
+        if filename not in self.jarfiles:
+          jarcontents = self.read(filename)
+          jarstream = StringIO(jarcontents)
+          jarfile = zipfile.ZipFile(jarstream)
+          self.jarfiles[filename] = jarfile
+        else:
+          jarfile = self.jarfiles[filename]
         yield filename, jarfile
 
-  def commonprefix(self):
-    """finds the common prefix of all the files stored in the jar files"""
-    filelist = []
-    for filename in self.iterfilenames():
-      filelist.append(filename.split('/'))
-    def cp(a, b):
-      l = min(len(a), len(b))
-      for n in range(l):
-        if a[n] != b[n]: return a[:n]
-      return a[:l]
-    return reduce(cp, filelist)
-
-  def iterfilenames(self):
+  def iterjarcontents(self):
     """iterate through all the localization files stored inside the jars"""
     for jarfilename, jarfile in self.iterjars():
       for filename in jarfile.namelist():
         if filename.endswith('/'): continue
         yield filename
 
+  def findcommonprefix(self):
+    """finds the common prefix of all the files stored in the jar files"""
+    return _commonprefix(self.iterjarcontents())
+
+  def stripcommonprefix(self, filename):
+    """strips the common prefix off the filename"""
+    fileparts = filename.split('/')
+    fileprefix = fileparts[:len(self.commonprefix)]
+    if fileprefix != self.commonprefix:
+      raise ValueError("cannot strip commonprefix %r from filename %r" % (self.commonprefix, filename))
+    return '/'.join(fileparts[len(self.commonprefix):])
+
+  def findjarprefixes(self):
+    """checks the uniqueness of the jar files contents"""
+    uniquenames = {}
+    jarprefixes = {}
+    for jarfilename, jarfile in self.iterjars():
+      jarprefixes[jarfilename] = ""
+      for filename in jarfile.namelist():
+        if filename.endswith('/'): continue
+        if filename in uniquenames:
+          jarprefixes[jarfilename] = True
+          jarprefixes[uniquenames[filename]] = True
+        else:
+          uniquenames[filename] = jarfilename
+    for jarfilename, hasconflicts in jarprefixes.items():
+      if hasconflicts:
+        shortjarfilename = os.path.split(jarfilename)[1]
+        shortjarfilename = os.path.splitext(shortjarfilename)[0]
+        jarprefixes[jarfilename] = shortjarfilename+'/'
+    return jarprefixes
+
+  def ziptoospath(self, zippath):
+    """converts a zipfile filepath to an os-style filepath"""
+    return os.path.join(*zippath.split('/'))
+
+  def ostozippath(self, ospath):
+    """converts an os-style filepath to a zipfile filepath"""
+    return '/'.join(ospath.split(os.path.pathsep))
+
   def iterextractnames(self, includenonjars=False):
     """iterates through all the localization files with the common prefix stripped and a jarfile name added if neccessary"""
-    jarcontents = {}
-    uniquenames = {}
-    hasconflicts = {}
     if includenonjars:
       nonjarfilelist = []
       for filename in self.namelist():
         if filename.endswith('/'): continue
         if not filename.lower().endswith(".jar"):
-          nonjarfilelist.append(filename.split('/'))
-      jarcontents[None] = nonjarfilelist
-    cplen = len(self.commonprefix())
+          yield self.ziptoospath(filename)
     for jarfilename, jarfile in self.iterjars():
-      jarfilelist = []
+      jarprefix = self.jarprefixes[jarfilename]
       for filename in jarfile.namelist():
         if filename.endswith('/'): continue
-        if filename in uniquenames:
-          hasconflicts[jarfilename] = True
-          hasconflicts[uniquenames[filename]] = True
-        else:
-          uniquenames[filename] = jarfilename
-        fileparts = filename.split('/')
-        jarfilelist.append(fileparts[cplen:])
-      jarcontents[jarfilename] = jarfilelist
-    for jarfilename, jarfilelist in jarcontents.iteritems():
-      if jarfilename in hasconflicts:
-        jarprefix = jarfilename[:jarfilename.lower().rfind('.jar')]
-        for fileparts in jarfilelist:
-          yield os.path.join(jarprefix, *fileparts)
-      else:
-        for fileparts in jarfilelist:
-          yield os.path.join(*fileparts)
+        yield self.ziptoospath(jarprefix+self.stripcommonprefix(filename))
 
 if __name__ == '__main__':
   try:
@@ -102,7 +132,6 @@ if __name__ == '__main__':
     optparser.error("need at least one argument")
   else:
     x = XpiFile(args[0])
-  x.printdir()
-  for name in x.iterextractnames():
+  for name in x.iterextractnames(True):
     print name
 
