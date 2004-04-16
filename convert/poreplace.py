8e3aa1493b23b135af728222fa0cc98d1b57f262
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2004-04-16 13:31:11 +0000

    added poreplace script as basis for tools to replace stuff inside po files
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@228 54714841-351b-0410-a198-e36a94b762f5

---
+++ convert/poreplace.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# 
+# Copyright 2002, 2003 Zuza Software Foundation
+# 
+# This file is part of translate.
+#
+# translate is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# translate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with translate; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""simple script to do replacements on translated strings inside po files"""
+
+# this is used as the basis for other scripts, it currently replaces nothing
+
+from translate.storage import po
+from translate.convert import convert
+
+class poreplace:
+  def convertstring(self, postr):
+    """does the conversion required on the given string (nothing in this case)"""
+    return postr
+
+  def convertfile(self, thepofile):
+    """goes through a po file and converts each element"""
+    for thepo in thepofile.poelements:
+      thepo.msgstr = [self.convertstring(postr) for postr in thepo.msgstr]
+    return thepofile
+
+  def convertpo(self, inputfile, outputfile, templatefile):
+    """reads in inputfile using po, converts using poreplace, writes to outputfile"""
+    # note that templatefile is not used, but it is required by the converter...
+    inputpo = po.pofile(inputfile)
+    if inputpo.isempty():
+      return 0
+    outputpo = self.convertfile(inputpo)
+    outputpolines = outputpo.tolines()
+    outputfile.writelines(outputpolines)
+    return 1
+
+def main(converterclass):
+  # handle command line options
+  from translate.convert import convert
+  inputformat = {"po":converterclass().convertpo}
+  outputformat = "po"
+  parser = convert.ConvertOptionParser(convert.optionalrecursion, inputformat, outputformat, usepots=True)
+  (options, args) = parser.parse_args()
+  # open the appropriate files
+  try:
+    parser.runconversion(options, converterclass().convertpo)
+  except convert.optparse.OptParseError, message:
+    parser.error(message)
+
+if __name__ == '__main__':
+  main(poreplace)
+
