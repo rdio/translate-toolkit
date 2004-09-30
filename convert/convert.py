d3ef2f5c54f67928691fda8384cdc4a5c627c3b7
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2004-09-30 09:52:01 +0000

    refactored as much as possible from MozConvertOptionParser classes in moz2po and po2moz the new class is convert.ArchiveConvertOptionParser, should be able to use for OO GSI files too... moz2po now just uses the class directly, po2moz adds one method this required some changes to the xpi.XpiFile class, basically it uses a few methods and iterators/in operator so should be simple to implement other archive classes...
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@575 54714841-351b-0410-a198-e36a94b762f5

--- convert/convert.py
+++ convert/convert.py
@@ -109,4 +109,169 @@ def copytemplate(inputfile, outputfile, templatefile, **kwargs):
   if not outputfile.isatty():
     outputfile.close()
   return True
-  
+
+# archive files need to know how to:
+# - openarchive: creates an archive object for the archivefilename
+#   * requires a constructor that takes the filename
+# - iterarchivefile: iterate through the names in the archivefile
+#   * requires the default iterator to do this
+# - archivefileexists: check if a given pathname exists inside the archivefile
+#   * uses the in operator - requires __contains__ (or will use __iter__ by default)
+# - openarchiveinputfile: returns an open input file from the archive, given the path
+#   * requires an archivefile.openinputfile method that takes the pathname
+# - openarchiveoutputfile: returns an open output file from the archive, given the path
+#   * requires an archivefile.openoutputfile method that takes the pathname
+
+class ArchiveConvertOptionParser(ConvertOptionParser):
+  """ConvertOptionParser that can handle recursing into single archive files. archiveformats maps extension to class"""
+  def __init__(self, formats, usetemplates=False, usepots=False, description=None, archiveformats={}):
+    self.archiveformats = archiveformats
+    ConvertOptionParser.__init__(self, formats, usetemplates, usepots, description=description)
+
+  def isrecursive(self, fileoption):
+    """checks if fileoption is a recursive file"""
+    if self.isarchive(fileoption): return True
+    return super(ArchiveConvertOptionParser, self).isrecursive(fileoption)
+
+  def isarchive(self, fileoption, mustexist=True):
+    """returns whether the file option is an archive file"""
+    if not isinstance(fileoption, (str, unicode)):
+      return False
+    if mustexist and not os.path.isfile(fileoption):
+      return False
+    fileext = self.splitext(fileoption)[1]
+    return fileext in self.archiveformats
+
+  def openarchive(self, archivefilename):
+    """creates an archive object for the given file"""
+    archiveext = self.splitext(archivefilename)[1]
+    archiveclass = self.archiveformats[archiveext]
+    return archiveclass(archivefilename)
+
+  def recurseinputfiles(self, options):
+    """recurse through archive file / directories and return files to be converted"""
+    if self.isarchive(options.input):
+      options.inputarchive = self.openarchive(options.input)
+      return self.recursearchivefiles(options)
+    else:
+      return super(ArchiveConvertOptionParser, self).recurseinputfiles(options)
+
+  def recursearchivefiles(self, options):
+    """recurse through archive files and convert files"""
+    inputfiles = []
+    for inputpath in options.inputarchive:
+      top, name = os.path.split(inputpath)
+      if not self.isvalidinputname(options, name):
+        continue
+      inputfiles.append(inputpath)
+    return inputfiles
+
+  def openinputfile(self, options, fullinputpath):
+    """opens the input file"""
+    if self.isarchive(options.input):
+      return options.inputarchive.openinputfile(fullinputpath)
+    else:
+      return super(ArchiveConvertOptionParser, self).openinputfile(options, fullinputpath)
+
+  def getfullinputpath(self, options, inputpath):
+    """gets the absolute path to an input file"""
+    if self.isarchive(options.input):
+      return inputpath
+    else:
+      return os.path.join(options.input, inputpath)
+
+  def opentemplatefile(self, options, fulltemplatepath):
+    """opens the template file (if required)"""
+    if fulltemplatepath is not None:
+      if self.isarchive(options.template):
+        # TODO: deal with different names in input/template archives
+        if fulltemplatepath in options.templatearchive:
+          return options.templatearchive.openinputfile(fulltemplatepath)
+        else:
+          self.warning("missing template file %s" % fulltemplatepath)
+    return super(ArchiveConvertOptionParser, self).opentemplatefile(options, fulltemplatepath)
+
+  def getfulltemplatepath(self, options, templatepath):
+    """gets the absolute path to a template file"""
+    if templatepath is not None and self.usetemplates and options.template:
+      if self.isarchive(options.template):
+        return templatepath
+      elif not options.recursivetemplate:
+        return templatepath
+      else:
+        return os.path.join(options.template, templatepath)
+    else:
+      return None
+
+  def templateexists(self, options, templatepath):
+    """returns whether the given template exists..."""
+    if templatepath is not None:
+      if self.isarchive(options.template):
+        # TODO: deal with different names in input/template archives
+        return templatepath in options.templatearchive
+    return super(ArchiveConvertOptionParser, self).templateexists(options, templatepath)
+
+  def getfulloutputpath(self, options, outputpath):
+    """gets the absolute path to an output file"""
+    if self.isarchive(options.output):
+      return outputpath
+    elif options.recursiveoutput and options.output:
+      return os.path.join(options.output, outputpath)
+    else:
+      return outputpath
+
+  def checkoutputsubdir(self, options, subdir):
+    """checks to see if subdir under options.output needs to be created, creates if neccessary"""
+    if not self.isarchive(options.output, mustexist=False):
+      super(ArchiveConvertOptionParser, self).checkoutputsubdir(options, subdir)
+
+  def openoutputfile(self, options, fulloutputpath):
+    """opens the output file"""
+    if self.isarchive(options.output, mustexist=False):
+      outputstream = options.outputarchive.openoutputfile(fulloutputpath)
+      if outputstream is None:
+        self.warning("Could not find where to put %s in output archive; writing to tmp" % fulloutputpath)
+        return StringIO()
+      return outputstream
+    else:
+      return super(ArchiveConvertOptionParser, self).openoutputfile(options, fulloutputpath)
+
+  def inittemplatearchive(self, options):
+    """opens the templatearchive if not already open"""
+    if options.template and self.isarchive(options.template) and not hasattr(options, "templatearchive"):
+      options.templatearchive = self.openarchive(options.template)
+
+  def recursiveprocess(self, options):
+    """recurse through directories and convert files"""
+    self.inittemplatearchive(options)
+    return super(ArchiveConvertOptionParser, self).recursiveprocess(options)
+
+  def processfile(self, fileprocessor, options, fullinputpath, fulloutputpath, fulltemplatepath):
+    """run an invidividual conversion"""
+    if self.isarchive(options.output, mustexist=False):
+      inputfile = self.openinputfile(options, fullinputpath)
+      # TODO: handle writing back to same archive as input/template
+      templatefile = self.opentemplatefile(options, fulltemplatepath)
+      outputfile = self.openoutputfile(options, fulloutputpath)
+      passthroughoptions = self.getpassthroughoptions(options)
+      if fileprocessor(inputfile, outputfile, templatefile, **passthroughoptions):
+        return True
+      else:
+        if fulloutputpath and os.path.isfile(fulloutputpath):
+          outputfile.close()
+          os.unlink(fulloutputpath)
+        return False
+    else:
+      return super(ArchiveConvertOptionParser, self).processfile(fileprocessor, options, fullinputpath, fulloutputpath, fulltemplatepath)
+
+  def splitinputext(self, inputpath):
+    """splits a inputpath into name and extension"""
+    # TODO: not sure if this should be here, was in po2moz
+    d, n = os.path.dirname(inputpath), os.path.basename(inputpath)
+    s = n.find(".")
+    if s == '-1':
+      return (inputpath, "")
+    root = os.path.join(d, n[:s])
+    ext = n[s+1:]
+    return (root, ext)
+
