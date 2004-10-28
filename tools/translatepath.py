4d195ebbf026fd2cf962d6af88de6bc3aff6d9dd
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2004-10-28 08:26:07 +0000

    removed all remaining translatepath stuff
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@656 54714841-351b-0410-a198-e36a94b762f5

--- tools/translatepath.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-try:
-  import translate
-except ImportError:
-  import sys
-  import os
-  translatepath=os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-  sys.path.append(translatepath)
-
