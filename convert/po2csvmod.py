c8fb5c8da2d68808a393ad0ce2ab566a23584978
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2004-11-04 14:11:14 +0000

    moved all the po2csv code into po2csvmod.py so that we can call it from pootle
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@687 54714841-351b-0410-a198-e36a94b762f5

---
+++ convert/po2csvmod.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# 
+# Copyright 2002, 2003 Zuza Software Foundation
+# 
+# This file is part of translate.
+#
+# translate is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# translate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with translate; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""simple script to convert a gettext .po localization file to a comma-separated values (.csv) file"""
+
+from translate.storage import po
+from translate.storage import csvl10n
+
+class po2csv:
+  def convertstring(self, postr):
+    unquotedstr = po.getunquotedstr(postr)
+    if len(unquotedstr) >= 1 and unquotedstr[:1] in "-+'": unquotedstr = "\\" + unquotedstr
+    return unquotedstr
+
+  def convertsource(self,thepo):
+    sourceparts = []
+    for sourcecomment in thepo.sourcecomments:
+      sourceparts.append(sourcecomment.replace("#:","",1).strip())
+    return " ".join(sourceparts)
+
+  def convertelement(self,thepo):
+    thecsv = csvl10n.csvelement()
+    if thepo.isheader():
+      thecsv.source = "source"
+      thecsv.msgid = "original"
+      thecsv.msgstr = "translation"
+    elif thepo.isblank():
+      return None
+    else:
+      thecsv.source = self.convertsource(thepo)
+      thecsv.msgid = self.convertstring(thepo.msgid)
+      # avoid plurals
+      msgstr = thepo.msgstr
+      if isinstance(msgstr, dict):
+        msgstr = thepo.msgstr[0]
+      thecsv.msgstr = self.convertstring(msgstr)
+    return thecsv
+
+  def convertplurals(self,thepo):
+    thecsv = csvl10n.csvelement()
+    thecsv.source = self.convertsource(thepo)
+    thecsv.msgid = self.convertstring(thepo.msgid_plural)
+    thecsv.msgstr = self.convertstring(thepo.msgstr[1])
+    return thecsv
+
+  def convertfile(self,thepofile):
+    thecsvfile = csvl10n.csvfile()
+    for thepo in thepofile.poelements:
+      thecsv = self.convertelement(thepo)
+      if thecsv is not None:
+        thecsvfile.csvelements.append(thecsv)
+      if thepo.hasplural():
+        thecsv = self.convertplurals(thepo)
+        if thecsv is not None:
+          thecsvfile.csvelements.append(thecsv)
+    return thecsvfile
+
+def convertcsv(inputfile, outputfile, templatefile):
+  """reads in inputfile using po, converts using po2csv, writes to outputfile"""
+  # note that templatefile is not used, but it is required by the converter...
+  inputpo = po.pofile(inputfile)
+  if inputpo.isempty():
+    return 0
+  convertor = po2csv()
+  outputcsv = convertor.convertfile(inputpo)
+  outputcsvlines = outputcsv.tolines()
+  outputfile.writelines(outputcsvlines)
+  return 1
+
