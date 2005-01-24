7d2b3d79f2474cafc86de3c8f84c6c4599b4525b
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2005-01-24 15:36:48 +0000

    moved some more registration / activation code into the users module where it belongs
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@1036 54714841-351b-0410-a198-e36a94b762f5

--- pootle/users.py
+++ pootle/users.py
@@ -4,6 +4,7 @@ from jToolkit.web import server
 from jToolkit.web import session
 from jToolkit.widgets import widgets
 from jToolkit.widgets import form
+from jToolkit import mailer
 from translate.pootle import pagelayout
 
 class RegistrationError(ValueError):
@@ -70,6 +71,112 @@ class OptionalLoginAppServer(server.LoginAppServer):
       session.pagecount += 1
       session.remote_ip = self.getremoteip(req)
     return self.getpage(pathwords, session, argdict)
+
+  def handleregistration(self, session, argdict):
+    """handles the actual registration"""
+    supportaddress = getattr(self.instance.registration, 'supportaddress', "")
+    username = argdict.get("username", "")
+    if not username or not username.isalnum() or not username[0].isalpha():
+      raise RegistrationError("Username must be alphanumeric, and must start with an alphabetic character")
+    email = argdict.get("email", "")
+    password = argdict.get("password", "")
+    if not (email and "@" in email and "." in email):
+      raise RegistrationError("You must supply a valid email address")
+    userexists = session.loginchecker.userexists(username)
+    if userexists:
+      usernode = getattr(session.loginchecker.users, username)
+      # use the email address on file
+      email = getattr(usernode, "email", email)
+      password = ""
+      # TODO: we can't figure out the password as we only store the md5sum. have a password reset mechanism
+      message = "You (or someone else) attempted to register an account with your username.\n"
+      message += "We don't store your actual password but only a hash of it\n"
+      if supportaddress:
+        message += "If you have a problem with registration, please contact %s\n" % supportaddress
+      else:
+        message += "If you have a problem with registration, please contact the site administrator\n"
+      displaymessage = "That username already exists. An email will be sent to the registered email address...\n"
+      redirecturl = "login.html?username=%s" % username
+      displaymessage += "Proceeding to <a href='%s'>login</a>\n" % redirecturl
+    else:
+      minpasswordlen = 6
+      if not password or len(password) < minpasswordlen:
+        raise RegistrationError("You must supply a valid password of at least %d characters" % minpasswordlen)
+      setattr(session.loginchecker.users, username + ".email", email)
+      setattr(session.loginchecker.users, username + ".passwdhash", session.md5hexdigest(password))
+      setattr(session.loginchecker.users, username + ".activated", 0)
+      activationcode = self.generateactivationcode()
+      setattr(session.loginchecker.users, username + ".activationcode", activationcode)
+      activationlink = ""
+      message = "A Pootle account has been created for you using this email address\n"
+      if session.instance.baseurl.startswith("http://"):
+        message += "To activate your account, follow this link:\n"
+        activationlink = session.instance.baseurl
+        if not activationlink.endswith("/"):
+          activationlink += "/"
+        activationlink += "activate.html?username=%s&activationcode=%s" % (username, activationcode)
+        message += "  %s  \n" % activationlink
+      message += "Your activation code is:\n%s\n" % activationcode
+      if activationlink:
+        message += "If you are unable to follow the link, please enter the above code at the activation page\n"
+      message += "This message is sent to verify that the email address is in fact correct. If you did not want to register an account, you may simply ignore the message.\n"
+      redirecturl = "activate.html?username=%s" % username
+      displaymessage = "Account created. You will be emailed login details and an activation code. Please enter your activation code on the <a href='%s'>activation page</a>. " % redirecturl
+      if activationlink:
+        displaymessage += "(Or simply click on the activation link in the email)"
+    self.saveuserprefs(session.loginchecker.users)
+    message += "Your user name is: %s\n" % username
+    if password.strip():
+      message += "Your password is: %s\n" % password
+    message += "Your registered email address is: %s\n" % email
+    smtpserver = self.instance.registration.smtpserver
+    fromaddress = self.instance.registration.fromaddress
+    messagedict = {"from": fromaddress, "to": [email], "subject": "Pootle Registration", "body": message}
+    if supportaddress:
+      messagedict["reply-to"] = supportaddress
+    fullmessage = mailer.makemessage(messagedict)
+    errmsg = mailer.dosendmessage(fromemail=self.instance.registration.fromaddress, recipientemails=[email], message=fullmessage, smtpserver=smtpserver)
+    if errmsg:
+      raise RegistrationError("Error sending mail: %s" % errmsg)
+    return displaymessage, redirecturl
+
+  def registerpage(self, session, argdict):
+    """handle registration or return the Register page"""
+    if "username" in argdict:
+      try:
+        displaymessage, redirecturl = self.handleregistration(session, argdict)
+      except RegistrationError, message:
+        session.status = str(message)
+        return RegisterPage(session, argdict)
+      message = pagelayout.IntroText(displaymessage)
+      redirectpage = pagelayout.PootlePage("Redirecting...", [message], session)
+      redirectpage.attribs["refresh"] = 10
+      redirectpage.attribs["refreshurl"] = redirecturl
+      return redirectpage
+    else:
+      return RegisterPage(session, argdict)
+
+  def activatepage(self, session, argdict):
+    """handle activation or return the Register page"""
+    if "username" in argdict and "activationcode" in argdict:
+      username = argdict["username"]
+      activationcode = argdict["activationcode"]
+      usernode = getattr(session.loginchecker.users, username, None)
+      if usernode is not None:
+        correctcode = getattr(usernode, "activationcode", "")
+        if correctcode and correctcode.strip().lower() == activationcode.strip().lower():
+          setattr(usernode, "activated", 1)
+          self.saveuserprefs(session.loginchecker.users)
+          redirecttext = pagelayout.IntroText("Your account has been activated! Redirecting to login...")
+          redirectpage = pagelayout.PootlePage("Redirecting to login...", redirecttext, session)
+          redirectpage.attribs["refresh"] = 10
+          redirectpage.attribs["refreshurl"] = "login.html?username=%s" % username
+          return redirectpage
+      failedtext = pagelayout.IntroText("The activation link you have entered was not valid")
+      failedpage = pagelayout.PootlePage("Activation Failed", failedtext, session)
+      return failedpage
+    else:
+      return ActivatePage(session, argdict)
 
 class ActivateSession(session.LoginSession):
   # TODO: refactor this into a LoginChecker somehow...
