5f53abf006515eb52976b3ae475567f09f8faca0
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2005-09-27 10:05:40 +0000

    greatly enhanced version moved things to a class-based structure allow diffing directories, and recursive diffs enabled a few GNU diff options that are useful fixed a few minor bugs tried to make the output similar to GNU diff
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@1604 54714841-351b-0410-a198-e36a94b762f5

--- tools/pydiff.py
+++ tools/pydiff.py
@@ -26,6 +26,7 @@ import optparse
 import time
 import os
 import sys
+import fnmatch
 
 lineterm = "\n"
 
@@ -33,76 +34,187 @@ def main():
     """main program for pydiff"""
     usage = "usage: %prog [options] fromfile tofile"
     parser = optparse.OptionParser(usage)
-    parser.add_option("-l", "--lines", type="int", default=3, help='Set number of context lines (default 3)')
-    parser.add_option("", "--fromcontains", type="string", default=None, help='Only include hunks where fromfile contains the given string')
-    parser.add_option("", "--tocontains", type="string", default=None, help='Only include hunks where tofile contains the given string')
-    parser.add_option("", "--contains", type="string", default=None, help='Only include hunks where fromfile or tofile contains the given string')
+    # GNU diff like options
+    parser.add_option("-i", "--ignore-case", default=False, action="store_true",
+            help='Ignore case differences in file contents.')
+    parser.add_option("-U", "--unified", type="int", metavar="NUM", default=3, dest="unified_lines",
+            help='Output NUM (default 3) lines of unified context')
+    parser.add_option("-r", "--recursive", default=False, action="store_true",
+            help='Recursively compare any subdirectories found.')
+    parser.add_option("-N", "--new-file", default=False, action="store_true",
+            help='Treat absent files as empty.')
+    parser.add_option("", "--unidirectional-new-file", default=False, action="store_true",
+            help='Treat absent first files as empty.')
+    parser.add_option("-s", "--report-identical-files", default=False, action="store_true",
+            help='Report when two files are the same.')
+    parser.add_option("-x", "--exclude", default=[], action="append", metavar="PAT",
+            help='Exclude files that match PAT.')
+    # our own options
+    parser.add_option("", "--fromcontains", type="string", default=None, metavar="TEXT",
+            help='Only show changes where fromfile contains TEXT')
+    parser.add_option("", "--tocontains", type="string", default=None, metavar="TEXT",
+            help='Only show changes where tofile contains TEXT')
+    parser.add_option("", "--contains", type="string", default=None, metavar="TEXT",
+            help='Only show changes where fromfile or tofile contains TEXT')
     (options, args) = parser.parse_args()
 
-    n = options.lines
     fromfile, tofile = args
+    if os.path.isdir(fromfile):
+        if os.path.isdir(tofile):
+            differ = DirDiffer(fromfile, tofile, options)
+        else:
+            parser.error("File %s is a directory while file %s is a regular file" % (fromfile, tofile))
+    else:
+        if os.path.isdir(tofile):
+            parser.error("File %s is a regular file while file %s is a directory" % (fromfile, tofile))
+        else:
+            differ = FileDiffer(fromfile, tofile, options)
+    differ.writediff(sys.stdout)
 
-    fromfiledate = time.ctime(os.stat(fromfile).st_mtime)
-    tofiledate = time.ctime(os.stat(tofile).st_mtime)
-    fromlines = open(fromfile, 'U').readlines()
-    tolines = open(tofile, 'U').readlines()
-    outfile = sys.stdout
+class DirDiffer:
+    """generates diffs between directories"""
+    def __init__(self, fromdir, todir, options):
+        """constructs a comparison between the two dirs using the given options"""
+        self.fromdir = fromdir
+        self.todir = todir
+        self.options = options
 
-    matcher = difflib.SequenceMatcher(None, fromlines, tolines)
-    groups = matcher.get_grouped_opcodes(n)
-    started = False
-    fromstring = '--- %s %s%s' % (fromfile, fromfiledate, lineterm)
-    tostring = '+++ %s %s%s' % (tofile, tofiledate, lineterm)
+    def isexcluded(self, difffile):
+        """checks if the given filename has been excluded from the diff"""
+        for exclude_pat in self.options.exclude:
+            if fnmatch.fnmatch(difffile, exclude_pat):
+                return True
+        return False
 
-    for group in groups:
-        hunk = "".join([line for line in unified_diff(group, fromlines, tolines)])
-        if options.fromcontains:
-            hunk_from_lines = "".join(get_from_lines(group, fromlines))
-            if options.fromcontains not in hunk_from_lines:
+    def writediff(self, outfile):
+        """writes the actual diff to the given file"""
+        fromfiles = os.listdir(self.fromdir)
+        tofiles = os.listdir(self.todir)
+        difffiles = dict.fromkeys(fromfiles + tofiles).keys()
+        difffiles.sort()
+        for difffile in difffiles:
+            if self.isexcluded(difffile):
                 continue
-        if options.tocontains:
-            hunk_to_lines = "".join(get_to_lines(group, tolines))
-            if options.tocontains not in hunk_to_lines:
+            from_ok = (difffile in fromfiles or self.options.new_file or self.options.unidirectional_new_file)
+            to_ok = (difffile in tofiles or self.options.new_file)
+            if from_ok and to_ok:
+                fromfile = os.path.join(self.fromdir, difffile)
+                tofile = os.path.join(self.todir, difffile)
+                if os.path.isdir(fromfile):
+                    if os.path.isdir(tofile):
+                        if self.options.recursive:
+                            differ = DirDiffer(fromfile, tofile, self.options)
+                            differ.writediff(outfile)
+                        else:
+                            outfile.write("Common subdirectories: %s and %s\n" % (fromfile, tofile))
+                    else:
+                        outfile.write("File %s is a directory while file %s is a regular file\n" % (fromfile, tofile))
+                else:
+                    if os.path.isdir(tofile):
+                        parser.error("File %s is a regular file while file %s is a directory\n" % (fromfile, tofile))
+                    else:
+                        filediffer = FileDiffer(fromfile, tofile, self.options)
+                        filediffer.writediff(outfile)
+            elif from_ok:
+                outfile.write("Only in %s: %s\n" % (self.fromdir, difffile))
+            elif to_ok:
+                outfile.write("Only in %s: %s\n" % (self.todir, difffile))
+                
+class FileDiffer:
+    """generates diffs between files"""
+    def __init__(self, fromfile, tofile, options):
+        """constructs a comparison between the two files using the given options"""
+        self.fromfile = fromfile
+        self.tofile = tofile
+        self.options = options
+
+    def writediff(self, outfile):
+        """writes the actual diff to the given file"""
+        validfiles = True
+        if os.path.exists(self.fromfile):
+            self.from_lines = open(self.fromfile, 'U').readlines()
+            fromfiledate = os.stat(self.fromfile).st_mtime
+        elif self.options.new_file or self.options.unidirectional_new_file:
+            self.from_lines = []
+            fromfiledate = 0
+        else:
+            outfile.write("%s: No such file or directory\n" % fromfile)
+            validfiles = False
+        if os.path.exists(self.tofile):
+            self.to_lines = open(self.tofile, 'U').readlines()
+            tofiledate = os.stat(self.tofile).st_mtime
+        elif self.options.new_file:
+            self.to_lines = []
+            tofiledate = 0
+        else:
+            outfile.write("%s: No such file or directory" % tofile)
+            validfiles = False
+        if not validfiles:
+            return
+        fromfiledate = time.ctime(fromfiledate)
+        tofiledate = time.ctime(tofiledate)
+        compare_from_lines = self.from_lines
+        compare_to_lines = self.to_lines
+        if self.options.ignore_case:
+            compare_from_lines = [line.lower() for line in compare_from_lines]
+            compare_to_lines = [line.lower() for line in compare_to_lines]
+        matcher = difflib.SequenceMatcher(None, compare_from_lines, compare_to_lines)
+        groups = matcher.get_grouped_opcodes(self.options.unified_lines)
+        started = False
+        fromstring = '--- %s\t%s%s' % (self.fromfile, fromfiledate, lineterm)
+        tostring = '+++ %s\t%s%s' % (self.tofile, tofiledate, lineterm)
+
+        for group in groups:
+            hunk = "".join([line for line in self.unified_diff(group)])
+            if self.options.fromcontains:
+                hunk_from_lines = "".join(self.get_from_lines(group))
+                if self.options.fromcontains not in hunk_from_lines:
+                    continue
+            if self.options.tocontains:
+                hunk_to_lines = "".join(self.get_to_lines(group))
+                if self.options.tocontains not in hunk_to_lines:
+                    continue
+            if self.options.contains:
+                hunk_lines = "".join(self.get_from_lines(group) + self.get_to_lines(group))
+                if self.options.contains not in hunk_lines:
+                    continue
+            if not started:
+                outfile.write(fromstring)
+                outfile.write(tostring)
+                started = True
+            outfile.write(hunk)
+        if not started and self.options.report_identical_files:
+            outfile.write("Files %s and %s are identical\n" % (self.fromfile, self.tofile))
+
+    def get_from_lines(self, group):
+        """returns the lines referred to by group, from the fromfile"""
+        from_lines = []
+        for tag, i1, i2, j1, j2 in group:
+            from_lines.extend(self.from_lines[i1:i2])
+        return from_lines
+    
+    def get_to_lines(self, group):
+        """returns the lines referred to by group, from the tofile"""
+        to_lines = []
+        for tag, i1, i2, j1, j2 in group:
+            to_lines.extend(self.to_lines[j1:j2])
+        return to_lines
+    
+    def unified_diff(self, group):
+        """takes the group of opcodes and generates a unified diff line by line"""
+        i1, i2, j1, j2 = group[0][1], group[-1][2], group[0][3], group[-1][4]
+        yield "@@ -%d,%d +%d,%d @@%s" % (i1+1, i2-i1, j1+1, j2-j1, lineterm)
+        for tag, i1, i2, j1, j2 in group:
+            if tag == 'equal':
+                for line in self.from_lines[i1:i2]:
+                    yield ' ' + line
                 continue
-        if options.contains:
-            hunk_lines = "".join(get_from_lines(group, fromlines) + get_to_lines(group, tolines))
-            if options.tocontains not in hunk_lines:
-                continue
-        if not started:
-            outfile.write(fromstring)
-            outfile.write(tostring)
-            started = True
-        outfile.write(hunk)
-
-def get_from_lines(group, a):
-    """returns the lines referred to by group, from the fromfile"""
-    from_lines = []
-    for tag, i1, i2, j1, j2 in group:
-        from_lines.extend(a[i1:i2])
-    return from_lines
-
-def get_to_lines(group, b):
-    """returns the lines referred to by group, from the tofile"""
-    to_lines = []
-    for tag, i1, i2, j1, j2 in group:
-        to_lines.extend(b[j1:j2])
-    return to_lines
-
-def unified_diff(group, a, b):
-    """takes the group of opcodes and generates a unified diff line by line"""
-    i1, i2, j1, j2 = group[0][1], group[-1][2], group[0][3], group[-1][4]
-    yield "@@ -%d,%d +%d,%d @@%s" % (i1+1, i2-i1, j1+1, j2-j1, lineterm)
-    for tag, i1, i2, j1, j2 in group:
-        if tag == 'equal':
-            for line in a[i1:i2]:
-                yield ' ' + line
-            continue
-        if tag == 'replace' or tag == 'delete':
-            for line in a[i1:i2]:
-                yield '-' + line
-        if tag == 'replace' or tag == 'insert':
-            for line in b[j1:j2]:
-                yield '+' + line
+            if tag == 'replace' or tag == 'delete':
+                for line in self.from_lines[i1:i2]:
+                    yield '-' + line
+            if tag == 'replace' or tag == 'insert':
+                for line in self.to_lines[j1:j2]:
+                    yield '+' + line
 
 if __name__ == "__main__":
     main()
