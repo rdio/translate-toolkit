f70825ea9828ed262ada5ef1e7fd606f6893b7ad
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2005-11-14 14:12:27 +0000

    added helper to return single element from pofile added doc strings to tests added test for bug 30
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@1671 54714841-351b-0410-a198-e36a94b762f5

--- convert/test_dtd2po.py
+++ convert/test_dtd2po.py
@@ -14,18 +14,32 @@ class TestDTD2PO:
         outputpo = convertor.convertfile(inputdtd)
         return outputpo
 
+    def singleelement(self, pofile):
+        """checks that the pofile contains a single non-header element, and returns it"""
+        assert len(pofile.poelements) == 2
+        assert pofile.poelements[0].isheader()
+        return pofile.poelements[1]
+
     def test_simpleentity(self):
+        """checks that a simple dtd entity definition converts properly to a po entry"""
         dtdsource = '<!ENTITY test.me "bananas for sale">\n'
         pofile = self.dtd2po(dtdsource)
-        assert len(pofile.poelements) == 2
-        poelement = pofile.poelements[1]
+        poelement = self.singleelement(pofile)
         assert po.unquotefrompo(poelement.msgid) == "bananas for sale"
         assert po.unquotefrompo(poelement.msgstr) == ""
 
     def test_emptyentity(self):
+        """bug 15 - checks that empty entity definitions survive into po file"""
         dtdsource = '<!ENTITY credit.translation "">\n'
         pofile = self.dtd2po(dtdsource)
-        assert len(pofile.poelements) == 2
-        poelement = pofile.poelements[1]
+        poelement = self.singleelement(pofile)
         assert "credit.translation" in "".join(poelement.tolines())
+
+    def test_entitynameincomment(self):
+        """bug 30 - error converting original dtd entity """
+        dtdsource = '<!--LOCALIZATION NOTE (editorCheck.label):\nDONT_TRANSLATE -->\n' + \
+            '<!ENTITY editorCheck.label "Composer">\n<!ENTITY editorCheck.accesskey "c">\n'
+        pofile = self.dtd2po(dtdsource)
+        poelement = self.singleelement(pofile)
+        assert '# DONT_TRANSLATE\n' in poelement.othercomments
 
