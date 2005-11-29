9cac5a79a89e9222be84d99d90fc48b206d9f98d
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2005-11-29 11:49:29 +0000

    added simple start to tests for po module
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@1687 54714841-351b-0410-a198-e36a94b762f5

---
+++ storage/test_po.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from translate.storage import po
+from translate.misc import wStringIO
+
+class TestPO:
+    def poparse(self, posource):
+        """helper that parses po source without requiring files"""
+        dummyfile = wStringIO.StringIO(posource)
+        pofile = po.pofile(dummyfile)
+        return pofile
+
+    def posource(self, pofile):
+        """helper that converts a po file back to source code"""
+        return "".join(pofile.tolines())
+
+    def poregen(self, posource):
+        """helper that converts po source to pofile object and back"""
+        return self.posource(self.poparse(posource))
+
+    def test_simpleentry(self):
+        """checks that a simple po entry is parsed correctly"""
+        posource = '#: test.c\nmsgid "test"\nmsgstr "rest"\n'
+        pofile = self.poparse(posource)
+        assert len(pofile.poelements) == 1
+        thepo = pofile.poelements[0]
+        assert thepo.getsources() == ["test.c"]
+        assert po.unquotefrompo(thepo.msgid) == "test"
+        assert po.unquotefrompo(thepo.msgstr) == "rest"
+
