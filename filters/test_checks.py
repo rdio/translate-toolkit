f51bf6c651add946f276236907d723bfc6aa4b3f
dwaynebailey <dwaynebailey@54714841-351b-0410-a198-e36a94b762f5>
2005-12-23 10:35:37 +0000

    Add a test for pofilter "long" test
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@1733 54714841-351b-0410-a198-e36a94b762f5

--- filters/test_checks.py
+++ filters/test_checks.py
@@ -81,6 +81,12 @@ def test_filepaths():
     assert checks.passes(stdchecker.filepaths, "%s to the file /etc/hosts on your system.", "%s na die leer /etc/hosts op jou systeem.")
     assert checks.fails(stdchecker.filepaths, "%s to the file /etc/hosts on your system.", "%s na die leer /etc/gasheer op jou systeem.")
     
+def test_long():
+    """tests long messages"""
+    stdchecker = checks.StandardChecker()
+    assert checks.passes(stdchecker.long, "I am normal", "Ek is ook normaal")
+    assert checks.fails(stdchecker.long, "Short.", "Kort.......................................................................................")
+
 def test_numbers():
     """test numbers"""
     stdchecker = checks.StandardChecker()
