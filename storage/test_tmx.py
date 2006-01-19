28a9b5e632dc6cf587ed4be896cde8e057a211e3
friedelwolff <friedelwolff@54714841-351b-0410-a198-e36a94b762f5>
2006-01-19 14:23:16 +0000

    Test entries with newlines and XML entities
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@1778 54714841-351b-0410-a198-e36a94b762f5

--- storage/test_tmx.py
+++ storage/test_tmx.py
@@ -17,8 +17,33 @@ class TestTMX:
         assert tmxfile.translate("A string of characters") == "'n String karakters"
 
     def test_addtranslation(self):
-        """tests that addtranslation stores strings correctly"""
+        """tests that addtranslation() stores strings correctly"""
         tmxfile = tmx.TmxParser()
         tmxfile.addtranslation("A string of characters", "en", "'n String karakters", "af")
         newfile = self.tmxparse(tmxfile.getxml())
+        print tmxfile.getxml()
         assert newfile.translate("A string of characters") == "'n String karakters"
+
+    def test_withnewlines(self):
+        """test addtranslation() with newlines"""
+        tmxfile = tmx.TmxParser()
+        tmxfile.addtranslation("First line\nSecond line", "en", "Eerste lyn\nTweede lyn", "af")
+        newfile = self.tmxparse(tmxfile.getxml())
+        print tmxfile.getxml()
+        assert newfile.translate("First line\nSecond line") == "Eerste lyn\nTweede lyn"
+
+    def test_xmlentities(self):
+        """Test that the xml entities '&' and '<'  are escaped correctly"""
+        tmxfile = tmx.TmxParser()
+        tmxfile.addtranslation("Mail & News", "en", "Nuus & pos", "af")
+        tmxfile.addtranslation("Five < ten", "en", "Vyf < tien", "af")
+        xmltext = tmxfile.getxml()
+        print "The generated xml:"
+        print xmltext
+        assert tmxfile.translate('Mail & News') == 'Nuus & pos'
+        assert xmltext.index('Mail &amp; News')
+        assert xmltext.find('Mail & News') == -1
+        assert tmxfile.translate('Five < ten') == 'Vyf < tien'
+        assert xmltext.index('Five &lt; ten')
+        assert xmltext.find('Five < ten') == -1
+
