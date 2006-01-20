a9e084f4ed72a8a867077d0a7fe55d73962be80c
dwaynebailey <dwaynebailey@54714841-351b-0410-a198-e36a94b762f5>
2006-01-20 10:05:34 +0000

    Add test for parsing a string or parsing a file.  Thought I had a bug but it slipped through :/.  The tests don't seem to find it.
    One problem the test writes to a file.  We should remove and destroy it, I
    didn't (know how to).
    
    
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@1781 54714841-351b-0410-a198-e36a94b762f5

--- storage/test_po.py
+++ storage/test_po.py
@@ -10,6 +10,13 @@ class TestPO:
         pofile = po.pofile(dummyfile)
         return pofile
 
+    def poparse_real(self, posource):
+        """helper that creates a real PO file for parsing"""
+        file = open("/tmp/joe.po", "w")
+        file.write(posource)
+	file.close()
+        return open("/tmp/joe.po", "r")
+
     def poregen(self, posource):
         """helper that converts po source to pofile object and back"""
         return str(self.poparse(posource))
@@ -67,4 +74,16 @@ class TestPO:
     def test_getunquotedstr(self):
         """checks that getunquotedstr works as advertised"""
         assert po.getunquotedstr(['"First line\nSecond line"'], includeescapes=False) == "First line\nSecond line"
+
+    def test_parse_source_string(self):
+        posource = '#: test.c\nmsgid "test"\nmsgstr "rest"\n'
+        pofile = self.poparse(posource)
+        assert len(pofile.poelements) == 1
+
+    def test_parse_real_file(self):
+        posource = '#: test.c\nmsgid "test"\nmsgstr "rest"\n'
+        tempfile = self.poparse_real(posource)
+        pofile = po.pofile(tempfile)
+        assert len(pofile.poelements) == 1
+	tempfile.close()
 
