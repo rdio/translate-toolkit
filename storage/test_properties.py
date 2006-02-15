b5d28120f0091d29c5b1fd639cefcf9b9c55ec5e
dwaynebailey <dwaynebailey@54714841-351b-0410-a198-e36a94b762f5>
2006-02-15 07:57:26 +0000

    Enable some tests that were disabled from 0.8 merging.  Make them pass.  Some are working but require playing with newlines.  Probably needs a better look within the properties class to distinguish between real translatables and others.
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@2027 54714841-351b-0410-a198-e36a94b762f5

--- storage/test_properties.py
+++ storage/test_properties.py
@@ -17,23 +17,24 @@ class TestProperties:
 
     def test_simpledefinition(self):
         """checks that a simple properties definition is parsed correctly"""
-        propsource = 'test_me=I can code!\n'
+        propsource = 'test_me=I can code!'
         propfile = self.propparse(propsource)
-        #XXX:failing: assert len(propfile.propelements) == 1
+        assert len(propfile.propelements) == 1
         propelement = propfile.propelements[0]
         assert propelement.name == "test_me"
         assert propelement.msgid == "I can code!"
 
     def test_simpledefinition_source(self):
         """checks that a simple properties definition can be regenerated as source"""
-        propsource = 'test_me=I can code!\n'
+        propsource = 'test_me=I can code!'
         propregen = self.propregen(propsource)
-        #XXX:failing: assert propsource == propregen
+        assert propsource + '\n' == propregen
 
     def test_unicode_escaping(self):
         """check that escapes unicode is converted properly"""
         propsource = "unicode=\u0411\u0416\u0419\u0428"
         propfile = self.propparse(propsource)
+        assert len(propfile.propelements) == 1
         propelement = propfile.propelements[0]
         assert propelement.name == "unicode"
         assert propelement.msgid.encode("UTF-8") == "БЖЙШ"
