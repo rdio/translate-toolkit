c570886dd261295364ea0bc9fb6edf1f2cccba84
charlvn <charlvn@54714841-351b-0410-a198-e36a94b762f5>
2006-02-17 12:10:44 +0000

    If the 'lang' attribute is placed on the 'html' element, '<head>' incorrectly becomes one of the translatable strings in the output.
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@2036 54714841-351b-0410-a198-e36a94b762f5

--- convert/test_html2po.py
+++ convert/test_html2po.py
@@ -18,3 +18,12 @@ class TestHTML2PO:
         markup = "<html><head><title>My title</title></head><body><h1>My heading</h1></body></html>"
         pofile = self.html2po(markup)
         assert len(pofile.units) > 0
+        
+    def test_htmllang(self):
+        """Test 'lang' attribute on 'html' element"""
+        markup = "<html lang=\"en\"><head><title>My title</title></head><body></body></html>"
+        badpo = "<head>"
+        pofile = self.html2po(markup)
+        print str(pofile)
+        assert len(pofile.units) > 0
+        assert str(pofile).find(badpo) == -1
