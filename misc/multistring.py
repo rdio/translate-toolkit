bb617a3169dd8530b3c86f85f049c060b85641a0
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2006-02-24 08:58:24 +0000

    added multistring type that stores alternate strings in a list
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@2051 54714841-351b-0410-a198-e36a94b762f5

---
+++ misc/multistring.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+"""Supports a hybrid Unicode string that can also have a list of alternate strings in the alt attribute"""
+
+from translate.misc import autoencode
+
+class multistring(autoencode.autoencode):
+    def __new__(newtype, string=u"", encoding=None, errors=None):
+        if isinstance(string, list):
+            if not string:
+                raise ValueError("multistring must contain at least one string")
+            mainstring = string[0]
+            newstring = multistring.__new__(newtype, string[0], encoding, errors)
+            newstring.alt = [autoencode.autoencode.__new__(autoencode.autoencode, altstring, encoding, errors) for altstring in string[1:]]
+        else:
+            newstring = autoencode.autoencode.__new__(newtype, string, encoding, errors)
+            newstring.alt = []
+        return newstring
+
+    def __init__(self, *args, **kwargs):
+        super(multistring, self).__init__(*args, **kwargs)
+        if not hasattr(self, "alt"):
+            self.alt = []    
+
+    def __cmp__(self, otherstring):
+        if isinstance(otherstring, multistring):
+            parentcompare = autoencode.autoencode.__cmp__(self, otherstring)
+            if parentcompare:
+                return parentcompare
+            else:
+                return cmp(self.alt, otherstring.alt)
+        elif isinstance(otherstring, autoencode.autoencode):
+            return autoencode.autoencode(self).__cmp__(otherstring)
+        elif isinstance(otherstring, unicode):
+            return unicode(self).__cmp__(otherstring)
+        elif isinstance(otherstring, str):
+            return cmp(str(self), otherstring)
+        else:
+            return cmp(basestring(self), otherstring)
+
+    def __repr__(self):
+        if not hasattr(self, "alt"):
+            print id(self), "has no alt"
+            return super(multistring, self).__repr__()
+        parts = [autoencode.autoencode.__repr__(self)] + [repr(a) for a in self.alt]
+        return "multistring(" + "/".join(parts) + ")"
+
---
+++ misc/test_multistring.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from translate.misc import multistring
+from translate.misc import test_autoencode
+
+class TestMultistring(test_autoencode.TestAutoencode):
+    type2test = multistring.multistring
+
+    def test_constructor(self):
+        t = self.type2test
+        s1 = t("test")
+        assert type(s1) == t
+        assert s1 == "test"
+        assert s1.alt == []
+        s2 = t(["test", "me"])
+        assert type(s2) == t
+        assert s2 == "test"
+        assert s2.alt == ["me"]
+        assert s2 != s1
+
