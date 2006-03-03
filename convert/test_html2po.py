cdc53c0b23041acc19378d7e503de4da2cd52660
charlvn <charlvn@54714841-351b-0410-a198-e36a94b762f5>
2006-03-03 12:47:53 +0000

    *** empty log message ***
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@2076 54714841-351b-0410-a198-e36a94b762f5

--- convert/test_html2po.py
+++ convert/test_html2po.py
@@ -13,17 +13,57 @@ class TestHTML2PO:
         outputpo = convertor.convertfile(inputfile, "test", False, False)
         return outputpo
         
-    def test_basic(self):
-        """Test basic conversion."""
-        markup = "<html><head><title>My title</title></head><body><h1>My heading</h1></body></html>"
-        pofile = self.html2po(markup)
-        assert len(pofile.units) > 0
-        
     def test_htmllang(self):
         """Test 'lang' attribute on 'html' element"""
         markup = "<html lang=\"en\"><head><title>My title</title></head><body></body></html>"
         badpo = "<head>"
         pofile = self.html2po(markup)
         print str(pofile)
-        assert len(pofile.units) > 0
+        assert len(pofile.units) == 1
         assert str(pofile).find(badpo) == -1
+        
+    def test_nolang(self):
+        """Test to see if text is extracted when there is no lang attribute"""
+        markup = "<html><head><title>My title</title></head><body><p>My text</p></body></html>"
+        badpo = "My text"
+        pofile = self.html2po(markup)
+        print str(pofile)
+        assert len(pofile.units) == 2
+        assert str(pofile).find(badpo) == -1
+        
+    def test_address(self):
+        """Test to see if the address element is extracted"""
+        markup = "<html lang=\"en\"><head><title>My title</title></head><body><address>My address</address></body></html>"
+        goodpo = "My address"
+        pofile = self.html2po(markup)
+        print str(pofile)
+        assert len(pofile.units) == 2
+        assert str(pofile).find(goodpo) > -1
+        
+    def test_headings(self):
+        """Test to see if the h4, h5 and h6 elements are extracted"""
+        markup = "<html lang=\"en\"><head><title>My title</title></head><body><h4>Heading Four</h4><h5>Heading Five</h5><h6>Heading Six</h6></body></html>"
+        pofile = self.html2po(markup)
+        print str(pofile)
+        assert len(pofile.units) == 4
+        assert str(pofile).find("Heading Four") > -1
+        assert str(pofile).find("Heading Five") > -1
+        assert str(pofile).find("Heading Six") > -1
+        
+    def test_dt(self):
+        """Test to see if the definition list title (dt) element is extracted"""
+        markup = "<html lang=\"en\"><head><title>My title</title></head><body><dl><dt>Definition List Item Title</dt></dl></body></html>"
+        goodpo = "Definition List Item Title"
+        pofile = self.html2po(markup)
+        print str(pofile)
+        assert len(pofile.units) == 2
+        assert str(pofile).find(goodpo) > -1
+        
+    def test_dd(self):
+        """Test to see if the definition list description (dd) element is extracted"""
+        markup = "<html lang=\"en\"><head><title>My title</title></head><body><dl><dd>Definition List Item Description</dd></dl></body></html>"
+        goodpo = "Definition List Item Description"
+        pofile = self.html2po(markup)
+        print str(pofile)
+        assert len(pofile.units) == 2
+        assert str(pofile).find(goodpo) > -1
