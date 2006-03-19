ccacdea8f70498f417ce7c3db9e5f72a6de3c514
dwaynebailey <dwaynebailey@54714841-351b-0410-a198-e36a94b762f5>
2006-03-19 13:08:39 +0000

    New test set for po2prop
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@2162 54714841-351b-0410-a198-e36a94b762f5

---
+++ convert/test_po2prop.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from translate.convert import po2prop
+from translate.convert import prop2po
+from translate.convert import test_convert
+from translate.misc import wStringIO
+from translate.storage import po
+from translate.storage import properties
+from py import test
+import warnings
+
+class TestPO2Prop:
+    def po2prop(self, posource):
+        """helper that converts po source to .properties source without requiring files"""
+        inputfile = wStringIO.StringIO(posource)
+        inputpo = po.pofile(inputfile)
+        convertor = po2prop.po2prop()
+        outputprop = convertor.convertfile(inputpo)
+        return outputprop
+
+    def merge2prop(self, propsource, posource):
+        """helper that merges po translations to .properties source without requiring files"""
+        inputfile = wStringIO.StringIO(posource)
+        inputpo = po.pofile(inputfile)
+        templatefile = wStringIO.StringIO(propsource)
+        #templateprop = properties.propfile(templatefile)
+        convertor = po2prop.reprop(templatefile)
+        outputprop = convertor.convertfile(inputpo)
+        print outputprop
+        return outputprop
+
+    def test_hard_newlines_preserved(self):
+        """check that we preserver hard coded newlines at the start and end of sentence"""
+        posource = '''#: prop\nmsgid "\\nvalue\\n\\n"\nmsgstr "\\nwaarde\\n\\n"\n'''
+        proptemplate = '''prop=\\nvalue\\n\\n\n'''
+        propexpected = '''prop=\\nwaarde\\n\\n\n'''
+        propfile = self.merge2prop(proptemplate, posource)
+        print propfile
+        assert propfile == [propexpected]
+
+class TestPO2PropCommand(test_convert.TestConvertCommand, TestPO2Prop):
+    """Tests running actual po2prop commands on files"""
+    convertmodule = po2prop
+    defaultoptions = {"progress": "none"}
+
