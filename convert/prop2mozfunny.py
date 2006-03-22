d04c20a78d5b6b19265fa5873ac98c959b2417fd
friedelwolff <friedelwolff@54714841-351b-0410-a198-e36a94b762f5>
2006-03-22 09:48:55 +0000

    fix syntax error with po2ini
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@2167 54714841-351b-0410-a198-e36a94b762f5

--- convert/prop2mozfunny.py
+++ convert/prop2mozfunny.py
@@ -101,8 +101,8 @@ def po2it(inputfile, outputfile, templatefile, encoding="cp1252", includefuzzy=F
       outputfile.write(line)
   return result
 
-def po2ini((inputfile, outputfile, templatefile, encoding="UTF-8", includefuzzy=False):
-  po2it(inputfile=inputfile, outputfile=outputfile, templatefile=templatefile, encoding=encoding, includefuzzy=includefuzzy):
+def po2ini(inputfile, outputfile, templatefile, encoding="UTF-8", includefuzzy=False):
+  po2it(inputfile=inputfile, outputfile=outputfile, templatefile=templatefile, encoding=encoding, includefuzzy=includefuzzy)
 
 def main(argv=None):
   import sys
