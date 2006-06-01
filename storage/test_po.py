e6188c4be347cac10fa24e6b1fd02995a15fbe3a
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2006-06-01 12:11:49 +0000

    added test that quoteforpo and unquotefrompo round string strings properly (strings same as in test_base.py)
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@2611 54714841-351b-0410-a198-e36a94b762f5

--- storage/test_po.py
+++ storage/test_po.py
@@ -6,6 +6,18 @@ from translate.storage import test_base
 from translate.misc.multistring import multistring
 from translate.misc import wStringIO
 
+def test_roundtrip_quoting():
+    specials = ['Fish & chips', 'five < six', 'six > five', 
+                'Use &nbsp;', 'Use &amp;nbsp;' 
+                'A "solution"', "skop 'n bal", '"""', "'''", 
+                '\n', '\t', '\r', 
+                '\\n', '\\t', '\\r', '\\"', '\r\n', '\\r\\n', '\\']
+    for special in specials:
+        quoted_special = po.quoteforpo(special)
+        unquoted_special = po.unquotefrompo(quoted_special)
+        print "special: %r\nquoted: %r\nunquoted: %r\n" % (special, quoted_special, unquoted_special)
+        assert special == unquoted_special
+
 class TestPOUnit(test_base.TestTranslationUnit):
     UnitClass = po.pounit
 
