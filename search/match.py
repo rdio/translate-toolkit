0932e26bfe3d466ec9e26ca9e58a39619f45602a
friedelwolff <friedelwolff@54714841-351b-0410-a198-e36a94b762f5>
2006-06-30 10:48:28 +0000

    Filter out usable units, convert to fast format, factor out (re)initialisation code
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@2921 54714841-351b-0410-a198-e36a94b762f5

--- search/match.py
+++ search/match.py
@@ -19,33 +19,56 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-"""Class to return likely matches from a given list of strings"""
+"""Class to perform translation memory matching from a store of translation units"""
 
 import Levenshtein
 import heapq
+from translate.storage import base
+
+def usable(unit):
+    """Returns whether this translation unit is usable for TM"""
+    #TODO: We might want to consider more attributes, such as approved, reviewed, etc.
+    if unit.source and unit.target and not unit.isfuzzy():
+        return True
+    return False
 
 class matcher:
     """A class that will do matching and store configuration for the matching process"""
-    def __init__(self, max_candidates=15, min_similarity=75, comparer=None):
+    def __init__(self, store, max_candidates=10, min_similarity=75, comparer=None):
         """max_candidates is the maximum number of candidates that should be assembled,
         min_similarity is the minimum similarity that must be attained to be included in
         the result, comparer is an optional Comparer with similarity() function"""
         if comparer is None:
             comparer = Levenshtein.LevenshteinComparer()
         self.comparer = comparer
+        self.setparameters(max_candidates, min_similarity)
+        self.inittm(store)
+        
+    def inittm(self, store):
+        """Initialises the memory for later use. We use simple base units for 
+        speedup."""
+        self.candidates = []
+        candidates = filter(usable, store.units)
+        for candidate in candidates:
+            simpleunit = base.TranslationUnit(candidate.source)
+            simpleunit.target = candidate.target
+            self.candidates.append(simpleunit)
+
+    def setparameters(self, max_candidates=10, min_similarity=75):
+        """Sets the parameters without reinitialising the tm. If a parameter 
+        is not specified, it is set to the default, not ignored"""
         self.MAX_CANDIDATES = max_candidates
         self.MIN_SIMILARITY = min_similarity
-
-    def matches(self, text, candidates):
+            
+    def matches(self, text):
         """Returns a list of possible matches for text in candidates with the associated similarity.
-        candidates is a list of base.TranslationUnits
         Return value is a list containing tuples (score, original, translation)."""
         bestcandidates = [(0.0,"","")]*self.MAX_CANDIDATES
         heapq.heapify(bestcandidates)
         #We use self.MIN_SIMILARITY, but if we already know we have max_candidates
         #that are better, we can adjust min_similarity upwards for speedup
         min_similarity = self.MIN_SIMILARITY
-        for candidate in candidates:
+        for candidate in self.candidates:
             cmpstring = candidate.source
             targetstring = candidate.target
             similarity = self.comparer.similarity(text, cmpstring, min_similarity)
