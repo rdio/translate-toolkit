9bdb72b23747080f1841fd1dc74389309a2f5e6b
dwaynebailey <dwaynebailey@54714841-351b-0410-a198-e36a94b762f5>
2006-07-23 07:48:35 +0000

    pychecker: vars -> variables as vars is builtin
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@3042 54714841-351b-0410-a198-e36a94b762f5

--- filters/test_decoration.py
+++ filters/test_decoration.py
@@ -7,16 +7,16 @@ from translate.filters import decoration
 def test_find_marked_variables():
     """check that we cna identify variables correctly, first value is start location, i
     second is avtual variable sans decoations"""
-    vars = decoration.findmarkedvariables("The <variable> string", "<", ">")
-    assert vars == [(4, "variable")]
-    vars = decoration.findmarkedvariables("The $variable string", "$", 1)
-    assert vars == [(4, "v")]
-    vars = decoration.findmarkedvariables("The $variable string", "$", None)
-    assert vars == [(4, "variable")]
-    vars = decoration.findmarkedvariables("The $variable string", "$", 0)
-    assert vars == [(4, "")]
-    vars = decoration.findmarkedvariables("The &variable; string", "&", ";")
-    assert vars == [(4, "variable")]
-    vars = decoration.findmarkedvariables("The &variable.variable; string", "&", ";")
-    assert vars == [(4, "variable.variable")]
+    variables = decoration.findmarkedvariables("The <variable> string", "<", ">")
+    assert variables == [(4, "variable")]
+    variables = decoration.findmarkedvariables("The $variable string", "$", 1)
+    assert variables == [(4, "v")]
+    variables = decoration.findmarkedvariables("The $variable string", "$", None)
+    assert variables == [(4, "variable")]
+    variables = decoration.findmarkedvariables("The $variable string", "$", 0)
+    assert variables == [(4, "")]
+    variables = decoration.findmarkedvariables("The &variable; string", "&", ";")
+    assert variables == [(4, "variable")]
+    variables = decoration.findmarkedvariables("The &variable.variable; string", "&", ";")
+    assert variables == [(4, "variable.variable")]
 
