57e5be051f187672754ff14d15d4ed54a0dc3f1a
friedelwolff <friedelwolff@54714841-351b-0410-a198-e36a94b762f5>
2006-08-15 06:45:03 +0000

    Extra xml test that should fail
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/translate@3256 54714841-351b-0410-a198-e36a94b762f5

--- filters/test_checks.py
+++ filters/test_checks.py
@@ -573,6 +573,7 @@ def test_xmltags():
     assert checks.fails(stdchecker.xmltags, "Click <a href=\"page.html\">", "Klik <a hverw=\"page.html\">")
     assert checks.passes(stdchecker.xmltags, "Click <a xml-lang=\"en\" href=\"page.html\">", "Klik <a xml-lang=\"af\" href=\"page.html\">")
     assert checks.fails(stdchecker.xmltags, "Click <a href=\"page.html\" target=\"koei\">", "Klik <a href=\"page.html\">")
+    assert checks.fails(stdchecker.xmltags, "<b>Current Translation</b>", "<b>Traducción Actual:<b>")
 
 def test_ooxmltags():
     """Tests the xml tags in OpenOffice.org translations for quality as done in gsicheck"""
