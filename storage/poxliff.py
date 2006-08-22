822f8c31c096fe1054b8a6583f47c676d7f05057
friedelwolff <friedelwolff@54714841-351b-0410-a198-e36a94b762f5>
2006-08-22 06:00:19 +0000

    Receive kwargs in constructor and set sourcelanguage to en-US if absent. Ignore sourcelanguage parameter in createfilenode
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk/translate@3757 54714841-351b-0410-a198-e36a94b762f5

--- storage/poxliff.py
+++ storage/poxliff.py
@@ -274,8 +274,15 @@ class PoXliffUnit(xliff.xliffunit):
 class PoXliffFile(xliff.xlifffile):
     """a file for the po variant of Xliff files"""
     UnitClass = PoXliffUnit
+    def __init__(self, *args, **kwargs):
+        if not "sourcelanguage" in kwargs:
+            kwargs["sourcelanguage"] = "en-US"
+        xliff.xlifffile.__init__(self, *args, **kwargs)
+
     def createfilenode(self, filename, sourcelanguage="en-US", datatype="po"):
-        return super(PoXliffFile, self).createfilenode(filename, sourcelanguage="en-US", datatype="po")
+        # Let's ignore the sourcelanguage parameter opting for the internal 
+        # one. PO files will probably be one language
+        return super(PoXliffFile, self).createfilenode(filename, sourcelanguage=self.sourcelanguage, datatype="po")
 
     def addheaderunit(self, target, filename):
         unit = self.addsourceunit(target, filename, True)
