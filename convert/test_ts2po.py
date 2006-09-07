d632565167b71ffddf2d806b96a072753fe6c332
friedelwolff <friedelwolff@54714841-351b-0410-a198-e36a94b762f5>
2006-09-07 09:37:15 +0000

    Test for basic conversion
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk/translate@3887 54714841-351b-0410-a198-e36a94b762f5

--- convert/test_ts2po.py
+++ convert/test_ts2po.py
@@ -7,7 +7,31 @@ from translate.storage import po
 from translate.storage import ts
 
 class TestTS2PO:
-    pass
+    def ts2po(self, tssource):
+        converter = ts2po.ts2po()
+        tsfile = wStringIO.StringIO(tssource)
+        outputpo = converter.convertfile(tsfile)
+        print "The generated po:"
+        print str(outputpo)
+        return outputpo
+        
+    def test_basic(self):
+        """tests basic conversion"""
+        tssource = '''<!DOCTYPE TS><TS>
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;Giới thiệu</translation>
+    </message>
+</context>
+</TS>
+'''
+        pofile = self.ts2po(tssource)
+        assert len(pofile.units) == 2
+        assert pofile.units[1].source == "&About"
+        assert pofile.units[1].target == "&Giới thiệu"
+        
 
 class TestTS2POCommand(test_convert.TestConvertCommand, TestTS2PO):
     """Tests running actual ts2po commands on files"""
