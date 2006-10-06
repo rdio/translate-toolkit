40f58b47ef2e4f737d7f01a92feade66c6833b7c
friedelwolff <friedelwolff@54714841-351b-0410-a198-e36a94b762f5>
2006-10-06 08:24:35 +0000

    Test for basic conversion and nonascii text
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk/translate@3995 54714841-351b-0410-a198-e36a94b762f5

--- convert/test_po2txt.py
+++ convert/test_po2txt.py
@@ -6,7 +6,28 @@ from translate.misc import wStringIO
 from translate.storage import po
 
 class TestPO2Txt:
-  pass
+    def po2txt(self, posource, txttemplate):
+        """helper that converts po source to txt source without requiring files"""
+        inputfile = wStringIO.StringIO(posource)
+        print inputfile.getvalue()
+        outputfile = wStringIO.StringIO()
+        templatefile = wStringIO.StringIO(txttemplate)
+        assert po2txt.converttxt(inputfile, outputfile, templatefile)
+        print outputfile.getvalue()
+        return outputfile.getvalue()
+
+    def test_basic(self):
+        """test basic conversion"""
+        txttemplate = "Heading\n\nBody text"
+        posource = 'msgid "Heading"\nmsgstr "Opskrif"\n\nmsgid "Body text"\nmsgstr "Lyfteks"\n'
+        assert self.po2txt(posource, txttemplate) == "Opskrif\n\nLyfteks"
+
+    def test_nonascii(self):
+        """test conversion with non-ascii text"""
+        txttemplate = "Heading\n\nFile content"
+        posource = 'msgid "Heading"\nmsgstr "Opskrif"\n\nmsgid "File content"\nmsgstr "Lêerinhoud"\n'
+        assert self.po2txt(posource, txttemplate) == "Opskrif\n\nLêerinhoud"
+
 
 class TestPO2TxtCommand(test_convert.TestConvertCommand, TestPO2Txt):
     """Tests running actual po2txt commands on files"""
