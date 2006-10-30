27b4e674d583b4bbab24caf257c2a9db7b870660
friedelwolff <friedelwolff@54714841-351b-0410-a198-e36a94b762f5>
2006-10-30 14:19:35 +0000

    Don't unnecessarily check for pofilter marked comment (.isreview() already does that)
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk/translate@4149 54714841-351b-0410-a198-e36a94b762f5

--- filters/pofilter.py
+++ filters/pofilter.py
@@ -78,7 +78,7 @@ class StandardPOChecker(POChecker):
 
   def isreview(self, thepo):
     """check if the po element has been marked review"""
-    return not (thepo.isreview() or thepo.hasmarkedcomment("pofilter"))
+    return not thepo.isreview()
 
 class pocheckfilter:
   def __init__(self, options, checkerclasses=None, checkerconfig=None):
