a14081c9d2cf9c0d8652b328873f587161447f30
andreaspauley <andreaspauley@54714841-351b-0410-a198-e36a94b762f5>
2006-12-06 12:38:27 +0000

    The storage factory can now guess the filetype by looking at the contents of a file. Implemented for xliff and po. Wrote some new factory tests and refactored the factory tests.
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk/translate@4420 54714841-351b-0410-a198-e36a94b762f5

--- storage/test_factory.py
+++ storage/test_factory.py
@@ -15,7 +15,7 @@ def givefile(filename, content):
     file.name = filename
     return file
 
-class Test_Factory:
+class BaseTestFactory:
     def test_getclass(self):
         assert classname("file.po") == "pofile"
         assert classname("file.pot") == "pofile"
@@ -29,9 +29,44 @@ class Test_Factory:
         assert not classname("file.po") == "tmxfile"
         assert not classname("file.po") == "xlifffile"
 
-    def test_poxliff(self):
-        """tests that we indead get a poxliff file, not just a normal one"""
-        poxlifffile = givefile("file.xliff", '''<?xml version="1.0" encoding="utf-8"?>
+    def test_getobject(self):
+        """Tests that we get a valid object."""
+        fileobj = givefile(self.filename, self.file_content)
+        store = factory.getobject(fileobj)
+        assert isinstance(store, self.expected_instance)
+        assert str(store) == self.file_content
+
+    def test_get_noname_object(self):
+        """Tests that we get a valid object from a file object without a name."""
+        fileobj = wStringIO.StringIO(self.file_content)
+        assert not hasattr(fileobj, 'name')
+        store = factory.getobject(fileobj)
+        assert str(store) == self.file_content
+
+class TestPOFactory(BaseTestFactory):
+    expected_instance = factory.po.pofile
+    filename = 'dummy.po'
+    file_content = '''#: test.c\nmsgid "test"\nmsgstr "rest"\n'''
+
+class TestXliffFactory(BaseTestFactory):
+    expected_instance = factory.xliff.xlifffile
+    filename = 'dummy.xliff'
+    file_content = '''<?xml version="1.0" encoding="utf-8"?>
+<xliff version="1.1" xmlns="urn:oasis:names:tc:xliff:document:1.1">
+<file>
+<body>
+  <trans-unit restype="x-gettext-domain-header">
+    <source>test</source>
+    <target>rest</target>
+  </trans-unit>
+</body>
+</file>
+</xliff>'''
+
+class TestPOXliffFactory(BaseTestFactory):
+    expected_instance = factory.poxliff.PoXliffFile
+    filename = 'dummy.xliff'
+    file_content = '''<?xml version="1.0" encoding="utf-8"?>
 <xliff version="1.1" xmlns="urn:oasis:names:tc:xliff:document:1.1">
 <file datatype="po" original="file.po" source-language="en-US"><body><trans-unit approved="no" id="1" restype="x-gettext-domain-header" xml:space="preserve">
 <source>MIME-Version: 1.0
@@ -42,8 +77,4 @@ Content-Transfer-Encoding: 8bit
 Content-Type: text/plain; charset=UTF-8
 Content-Transfer-Encoding: 8bit
 </target>
-</trans-unit></body></file></xliff>''')
-        
-        assert classname(poxlifffile) == "xlifffile"
-        xliffobject = factory.getobject(poxlifffile)
-        assert xliffobject.__class__.__name__.lower() == "poxlifffile"
+</trans-unit></body></file></xliff>'''
