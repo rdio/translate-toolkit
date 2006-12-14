8674099d757d8f4ef9bb57f39828293bdb16ec79
dwaynebailey <dwaynebailey@54714841-351b-0410-a198-e36a94b762f5>
2006-12-14 12:15:04 +0000

    Add some new tests
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk/translate@4541 54714841-351b-0410-a198-e36a94b762f5

--- convert/test_txt2po.py
+++ convert/test_txt2po.py
@@ -6,7 +6,28 @@ from translate.misc import wStringIO
 from translate.storage import po
 
 class TestTxt2PO:
-  pass
+    def txt2po(self, txtsource, template=None):
+        """helper that converts txt source to po source without requiring files"""
+        inputfile = wStringIO.StringIO(txtsource)
+        convertor = txt2po.txt2po()
+        outputpo = convertor.convertfile(inputfile, "", False)
+        return str(outputpo)
+
+    def singleelement(self, storage):
+        """checks that the pofile contains a single non-header element, and returns it"""
+        print storage.getoutput()
+        assert len(storage.units) == 1
+        return storage.units[0]
+
+    def test_simple(self):
+        """test the most basic txt conversion"""
+        txtsource = "A simple string"
+        poexpected = '''#: :1
+msgid "A simple string"
+msgstr ""
+'''
+        poresult = self.txt2po(txtsource)
+        assert poresult == poexpected
 
 class TestTxt2POCommand(test_convert.TestConvertCommand, TestTxt2PO):
     """Tests running actual txt2po commands on files"""
