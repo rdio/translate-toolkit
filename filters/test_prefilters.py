28658dff4787b5de64f9febc4e685c10b3adfb8b
dwaynebailey <dwaynebailey@54714841-351b-0410-a198-e36a94b762f5>
2008-05-11 19:57:30 +0000

    Make filterwordwithpunctuation only work with Unicode strings
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/src/trunk/translate@7397 54714841-351b-0410-a198-e36a94b762f5

--- filters/test_prefilters.py
+++ filters/test_prefilters.py
@@ -6,15 +6,15 @@
 from translate.filters import prefilters
 
 def test_filterwordswithpunctuation():
-    string = "Nothing in here."
+    string = u"Nothing in here."
     filtered = prefilters.filterwordswithpunctuation(string)
     assert filtered == string
     # test listed words (start / end with apostrophe)
-    string = "'n Boom het 'n tak."
+    string = u"'n Boom het 'n tak."
     filtered = prefilters.filterwordswithpunctuation(string)
     assert filtered == "n Boom het n tak."
     # test words containing apostrophe
-    string = "It's in it's own place."
+    string = u"It's in it's own place."
     filtered = prefilters.filterwordswithpunctuation(string)
     assert filtered == "Its in its own place."
     # test strings in unicode
